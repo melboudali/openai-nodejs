24e7accdd2f4d46040a0932e586e1413b80e1225
melboudali <med.sneaky@gmail.com>
2024-01-09 00:56:58 +0000

    added movies data

---
+++ data.js
@@ -0,0 +1,67 @@
+export const movies = [
+    {
+        id: 1,
+        title: "The Shawshank Redemption",
+        description: "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion."
+    },
+    {
+        id: 2,
+        title: "The Godfather",
+        description: "Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger."
+    },
+    {
+        id: 3,
+        title: "The Dark Knight",
+        description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."
+    },
+    {
+        id: 4,
+        title: "The Lord of the Rings: The Return of the King",
+        description: "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring."
+    },
+    {
+        id: 5,
+        title: "Pulp Fiction",
+        description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption."
+    },
+    {
+        id: 6,
+        title: "Forrest Gump",
+        description: "The history of the United States from the 1950s to the '70s unfolds from the perspective of an Alabama man with an IQ of 75, who yearns to be reunited with his childhood sweetheart."
+    },
+    {
+        id: 7,
+        title: "Fight Club",
+        description: "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more."
+    },
+    {
+        id: 8,
+        title: "Inception",
+        description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster."
+    },
+    {
+        id: 9,
+        title: "The Matrix",
+        description: "When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence."
+    },
+    {
+        id: 10,
+        title: "Goodfellas",
+        description: "The story of Henry Hill and his life in the mafia, covering his relationship with his wife Karen and his mob partners Jimmy Conway and Tommy DeVito."
+    },
+    {
+        id: 11,
+        title: "Interstellar",
+        description: "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans."
+    },
+    {
+        id: 12,
+        title: "Spider-Man: Across the Spider-Verse",
+        description: "Miles Morales catapults across the multiverse, where he encounters a team of Spider-People charged with protecting its very existence. When the heroes clash on how to handle a new threat, Miles must redefine what it means to be a hero."
+    },
+        {
+        id: 13,
+        title: "Gladiator",
+        description: "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery."
+    },
+]
